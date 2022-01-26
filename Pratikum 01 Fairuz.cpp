8314957bb574c7964a1c6319bfd0a82f6b8e8a23
Fairuz10 <98435735+Fairuz10@users.noreply.github.com>
2022-01-26 07:52:49 +0700

    Add files via upload

---
+++ Pratikum 01 Fairuz.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+		
+		printf("\nNIM	: 07.1.2021.0035");
+		
+		printf("\nNAMA	: Fairuz Ariqoh Rizqulloh");
+		
+		printf("\nJurusan	: Teknik Informatika");
+			
+			return 0;
+}
